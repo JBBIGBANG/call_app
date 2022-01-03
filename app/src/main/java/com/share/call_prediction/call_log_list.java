8e272bdd9fe1e6e817a4136571b917cc29028ba2
Joy Sarkar <sjoy.cse3.bu@gmail.com>
2022-01-03 08:55:21 +0600

    get api complete

--- app/src/main/java/com/share/call_prediction/call_log_list.java
+++ app/src/main/java/com/share/call_prediction/call_log_list.java
@@ -11,6 +11,13 @@ import android.telecom.Call;
 import android.view.View;
 import android.widget.TextView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
 import java.util.ArrayList;
 
 public class call_log_list extends AppCompatActivity {
@@ -56,6 +63,31 @@ public class call_log_list extends AppCompatActivity {
         }
 
         System.out.println(callInfos.size());
-        textView.setText(stringOutput);
+//        textView.setText(stringOutput);
+        apiCall();
+    }
+
+    private void apiCall(){
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url ="https://catfact.ninja/fact";
+
+// Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Display the first 500 characters of the response string.
+                        textView.setText("Response is: "+ response);
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                textView.setText("That didn't work!");
+            }
+        });
+
+// Add the request to the RequestQueue.
+        queue.add(stringRequest);
+
     }
 }
